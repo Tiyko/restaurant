9b4cbfdafe67a57067114ac30092e78ffc8fbfa4
Tiyko <johnny86shepherd@gmail.com>
2022-09-21 18:07:34 +0000

    add urls and made migrations

--- luigis_castle_pizza/urls.py
+++ luigis_castle_pizza/urls.py
@@ -14,10 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from restaurant import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.restaurant, name='index.html'),
+    path('summernote/', include('django_summernote.urls')),
+    # path('', views.restaurant, name='index.html'),
 ]
