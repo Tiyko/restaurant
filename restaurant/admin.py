a06d558c88049798b5537d7334f43ffe018c01c5
Tiyko <johnny86shepherd@gmail.com>
2022-09-25 19:04:54 +0000

    add admin panel views

--- restaurant/admin.py
+++ restaurant/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django_summernote.admin import SummernoteModelAdmin
-from .models import Post, Comment
+from .models import Post, Comment, Reservation, Address, Orders, Menu, Items
 
 
 @admin.register(Post)
@@ -15,6 +15,7 @@ class PostAdmin(SummernoteModelAdmin):
 
 @admin.register(Comment)
 class CommentAdmin(admin.ModelAdmin):
+
     list_display = ('name', 'body', 'post', 'created_on', 'approved')
     list_filter = ('approved', 'created_on')
     search_fields = ('name', 'email', 'body')
@@ -22,3 +23,55 @@ class CommentAdmin(admin.ModelAdmin):
 
     def approve_comments(self, request, queryset):
         queryset.update(approved=True)
+
+
+@admin.register(Reservation)
+class ReservationAdmin(admin.ModelAdmin):
+
+    list_display = ['id', 'username', 'reservation_date', 'created_on']
+    search_fields = ['username', 'id']
+    list_filter = ['created_on']
+
+
+@admin.register(Address)
+class AddressAdmin(admin.ModelAdmin):
+
+    list_display = ['id', 'username', 'address', 'zipcode', 'created_on']
+    search_fields = ['username', 'id']
+    list_filter = ['created_on']
+
+
+@admin.register(Orders)
+class OrdersAdmin(admin.ModelAdmin):
+
+    list_display = ['id', 'username', 'total_price', 'created_on']
+    search_fields = ['username', 'id']
+    list_filter = ['created_on']
+
+
+@admin.register(Menu)
+class MenuAdmin(admin.ModelAdmin):
+
+    list_display = ['id', 'meal_name', 'meal_description', 'price', 'imageUrl', 'created_on']
+    search_fields = ['username', 'id']
+    list_filter = ['created_on']
+
+
+@admin.register(Items)
+class ItemsAdmin(admin.ModelAdmin):
+
+    list_display = ['id', 'meal_name', 'menu_id', 'order_id', 'quantity', 'price_menu', 'created_on']
+    search_fields = ['id']
+    list_filter = ['created_on']
+
+    def meal_name(self, obj):
+        return obj.id_menu.meal_name
+
+    def menu_id(self, obj):
+        return obj.id_menu.id
+
+    def order_id(self, obj):
+        return obj.order.id
+
+    def price_menu(self, obj):
+        return obj.id_menu.price
