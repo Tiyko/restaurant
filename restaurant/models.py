6051306dbd746999536cef1a21c80c970b0eac44
Tiyko <johnny86shepherd@gmail.com>
2022-09-25 19:04:40 +0000

    add models

--- restaurant/models.py
+++ restaurant/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
+from datetime import datetime
 
 
 STATUS = ((0, "Draft"), (1, "Published"))
@@ -10,7 +11,7 @@ class Post(models.Model):
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
     author = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name="blog_posts")
+        User, on_delete=models.CASCADE, related_name="meal_posts")
     featured_image = CloudinaryField('image', default='placeholder')
     excerpt = models.TextField(blank=True)
     updated_on = models.DateTimeField(auto_now=True)
@@ -18,7 +19,7 @@ class Post(models.Model):
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
     likes = models.ManyToManyField(
-        User, related_name='blogpost_like', blank=True)
+        User, related_name='meal_like', blank=True)
 
     class Meta:
         ordering = ["-created_on"]
@@ -44,3 +45,59 @@ class Comment(models.Model):
 
     def __str__(self):
         return f"Comment {self.body} by {self.name}"
+
+
+class Reservation(models.Model):
+    id = models.AutoField(primary_key=True, unique=True)
+    username = models.ForeignKey(User, on_delete=models.CASCADE)
+    reservation_date = models.DateTimeField(default=datetime.now, blank=True)
+    created_on = models.DateTimeField(auto_now_add=True)
+
+
+class Address(models.Model):
+    id = models.AutoField(primary_key=True, unique=True)
+    username = models.ForeignKey(User, on_delete=models.CASCADE)
+    address = models.CharField(max_length=200)
+    zipcode = models.CharField(max_length=20)
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name = 'Address'
+        verbose_name_plural = 'Addresses'
+
+
+class Orders(models.Model):
+    id = models.AutoField(primary_key=True, unique=True)
+    username = models.ForeignKey(User, on_delete=models.CASCADE)
+    total_price = models.FloatField()
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name = 'Order'
+        verbose_name_plural = 'Orders'
+
+
+class Menu(models.Model):
+    id = models.AutoField(primary_key=True, unique=True)
+    meal_name = models.CharField(max_length=200)
+    meal_description = models.CharField(max_length=500)
+    imageUrl = CloudinaryField('image', default='placeholder')
+    price = models.FloatField()
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name = 'Menu'
+        verbose_name_plural = 'Menu'
+
+
+class Items(models.Model):
+    id = models.AutoField(primary_key=True, unique=True)
+    id_menu = models.ForeignKey(Menu, on_delete=models.CASCADE)
+    order = models.ForeignKey(Orders, on_delete=models.CASCADE)
+    quantity = models.IntegerField()
+    price = models.FloatField()
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name = 'Items'
+        verbose_name_plural = 'Items'
