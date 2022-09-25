4af567b2e609635898020a515de7dcdf4fd85564
Tiyko <johnny86shepherd@gmail.com>
2022-09-25 19:24:35 +0000

    add django-allauth

--- luigis_castle_pizza/urls.py
+++ luigis_castle_pizza/urls.py
@@ -20,5 +20,6 @@ from restaurant import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('summernote/', include('django_summernote.urls')),
+    path('accounts/', include('allauth.urls')),
     # path('', views.restaurant, name='index.html'),
 ]
